b9ad1c546e5b91958d66353f70e607567ebfd6b4
52fancy <575798563@qq.com>
2024-03-14 17:36:44 +0800

    cf-ddns.sh

--- cf-ddns.sh
+++ cf-ddns.sh
@@ -18,7 +18,7 @@ set -o nounset
 #            -u user@example.com \
 #            -h host.example.com \     # fqdn of the record you want to update
 #            -z example.com \          # will show you all zones if forgot, but you need this
-#            -t A|AAAA|All                 # specify ipv4/ipv6/All, default: All
+#            -t A|AAAA|Both                 # specify ipv4/ipv6/Both, default: Both
 
 # default config
 
@@ -35,35 +35,35 @@ CFZONE_NAME=
 # Hostname to update, eg: ddns.example.com
 CFRECORD_NAME=
 
-# Record type, A(IPv4)|AAAA(IPv6)|All(IPv4+IPv6), default All
-CFRECORD_TYPE=All
+# Record type, A(IPv4)|AAAA(IPv6)|Both(IPv4+IPv6), default Both
+CFRECORD_TYPE=Both
 
 # If required settings are missing just exit
 if [ "$CFKEY" = "" ]; then
-  echo "Missing api-key, get at: https://www.cloudflare.com/a/account/my-account"
-  echo "and save in ${0} or using the -k flag"
-  exit 2
+    echo "Missing api-key, get at: https://www.cloudflare.com/a/account/my-account"
+    echo "and save in ${0} or using the -k flag"
+    exit 2
 fi
 if [ "$CFUSER" = "" ]; then
-  echo "Missing username, probably your email-address"
-  echo "and save in ${0} or using the -u flag"
-  exit 2
+    echo "Missing username, probably your email-address"
+    echo "and save in ${0} or using the -u flag"
+    exit 2
 fi
 if [ "$CFRECORD_NAME" = "" ]; then 
-  echo "Missing hostname, eg: ddns.example.com"
-  echo "save in ${0} or using the -h flag"
-  exit 2
+    echo "Missing hostname, eg: ddns.example.com"
+    echo "save in ${0} or using the -h flag"
+    exit 2
 fi
 
 if [ "$CFZONE_NAME" = "" ]; then 
-  echo "Missing Zone name, eg: example.com"
-  echo "save in ${0} or using the -z flag"
-  exit 2
+    echo "Missing Zone name, eg: example.com"
+    echo "save in ${0} or using the -z flag"
+    exit 2
 fi
 # If the hostname is not a FQDN
 if [ "$CFRECORD_NAME" != "$CFZONE_NAME" ] && ! [ -z "${CFRECORD_NAME##*$CFZONE_NAME}" ]; then
-  CFRECORD_NAME="$CFRECORD_NAME.$CFZONE_NAME"
-  echo " => Hostname is not a FQDN, assuming $CFRECORD_NAME"
+    CFRECORD_NAME="$CFRECORD_NAME.$CFZONE_NAME"
+    echo " => Hostname is not a FQDN, assuming $CFRECORD_NAME"
 fi
 
 
@@ -78,49 +78,52 @@ while getopts k:u:h:z:t: opts; do
   esac
 done
 
-CFZONE_ID=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones?name=$CFZONE_NAME" -H "X-Auth-Email: $CFUSER" -H "X-Auth-Key: $CFKEY" -H "Content-Type: application/json" | grep -Po '(?<="id":")[^"]*' | head -1)
-
-getUpdate_V4(){
-    WANIP="$(curl -s http://ipv4.icanhazip.com)"
-	CFRECORD_ID=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/$CFZONE_ID/dns_records?name=$CFRECORD_NAME" -H "X-Auth-Email: $CFUSER" -H "X-Auth-Key: $CFKEY" -H "Content-Type: application/json"  | grep -Po '(?<="id":")[^"]*' | head -1)
-	RESPONSE=$(curl -s -X PUT "https://api.cloudflare.com/client/v4/zones/$CFZONE_ID/dns_records/$CFRECORD_ID" \
+getWANIP(){
+    CFRECORD_TYPE=$1
+    
+    if [ "$CFRECORD_TYPE" = "AAAA" ]; then
+        WANIP="$(curl -s http://ipv6.icanhazip.com)"
+    else
+        WANIP="$(curl -s http://ipv4.icanhazip.com)"
+    fi
+    echo "$WANIP"
+}
+getUpdate(){
+    CFKEY=$1
+    CFUSER=$2
+    CFZONE_NAME=$3
+    CFRECORD_NAME=$4
+    CFRECORD_TYPE=$5
+    WANIP=$6
+    
+    CFZONE_ID=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones?name=$CFZONE_NAME" -H "X-Auth-Email: $CFUSER" -H "X-Auth-Key: $CFKEY" -H "Content-Type: application/json" | grep -Po '(?<="id":")[^"]*' | head -1)
+   
+    CFRECORD_ID=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/$CFZONE_ID/dns_records?name=$CFRECORD_NAME&type=$CFRECORD_TYPE" -H "X-Auth-Email: $CFUSER" -H "X-Auth-Key: $CFKEY" -H "Content-Type: application/json"  | grep -Po '(?<="id":")[^"]*')
+    RESPONSE=$(curl -s -X PUT "https://api.cloudflare.com/client/v4/zones/$CFZONE_ID/dns_records/$CFRECORD_ID" \
         -H "X-Auth-Email: $CFUSER" \
         -H "X-Auth-Key: $CFKEY" \
         -H "Content-Type: application/json" \
-        --data "{\"type\":\"A\",\"name\":\"$CFRECORD_NAME\",\"content\":\"$WANIP\"}")
+        --data "{\"type\":\"$CFRECORD_TYPE\",\"name\":\"$CFRECORD_NAME\",\"content\":\"$WANIP\"}")
     if [ "$RESPONSE" != "${RESPONSE%success*}" ] && [ "$(echo $RESPONSE | grep "\"success\":true")" != "" ]; then
-        echo "Updated ipv4 succesfuly!"
-        echo $WANIP
-	else
+        echo "Updated succesfuly!"
+        echo "$WANIP"
+    else
         echo 'Something went wrong :('
         echo "Response: $RESPONSE"
-    fi		
+    fi      
 }
 
-getUpdate_V6(){
-	WANIP="$(curl -s http://ipv6.icanhazip.com)"
-	CFRECORD_ID=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/$CFZONE_ID/dns_records?name=$CFRECORD_NAME" -H "X-Auth-Email: $CFUSER" -H "X-Auth-Key: $CFKEY" -H "Content-Type: application/json"  | grep -Po '(?<="id":")[^"]*' | tail -1)
-	RESPONSE=$(curl -s -X PUT "https://api.cloudflare.com/client/v4/zones/$CFZONE_ID/dns_records/$CFRECORD_ID" \
-        -H "X-Auth-Email: $CFUSER" \
-        -H "X-Auth-Key: $CFKEY" \
-        -H "Content-Type: application/json" \
-        --data "{\"type\":\"AAAA\",\"name\":\"$CFRECORD_NAME\",\"content\":\"$WANIP\"}")
-    if [ "$RESPONSE" != "${RESPONSE%success*}" ] && [ "$(echo $RESPONSE | grep "\"success\":true")" != "" ]; then
-        echo "Updated ipv6 succesfuly!"
-        echo $WANIP
-	else
-        echo 'Something went wrong :('
-        echo "Response: $RESPONSE"
-    fi		
-}
+if [ "$CFRECORD_TYPE" = "AAAA" ] || [ "$CFRECORD_TYPE" = "Both" ]; then
+    IPV6="$(getWANIP "AAAA")"
+fi
+if [ "$CFRECORD_TYPE" = "A" ] || [ "$CFRECORD_TYPE" = "Both" ]; then
+    IPV4="$(getWANIP "A")"
+fi
 
-if [ "$CFRECORD_TYPE" = "A" ]; then
-	getUpdate_V4
-elif [ "$CFRECORD_TYPE" = "AAAA" ]; then
-	getUpdate_V6
-else
-	getUpdate_V4
-	sleep 5
-	getUpdate_V6
+if [ "$IPV6" != "" ]; then
+    getUpdate $CFKEY $CFUSER $CFZONE_NAME $CFRECORD_NAME "AAAA" $IPV6
+fi
+if [ "$IPV4" != "" ]; then
+    getUpdate $CFKEY $CFUSER $CFZONE_NAME $CFRECORD_NAME "A" $IPV4
 fi
 exit 0
